00ee56691e7cee02ad43dbe77b68b3fff632ff1c
littleprogrammer-r5r5 <68316674+littleprogrammer-r5r5@users.noreply.github.com>
2020-08-26 05:53:56 +0700

    Delete read_file.py

--- expression_tree/read_file.py
+++
@@ -1,8 +0,0 @@
-#Read function
-def read_file(PATH):
-    text = open(PATH, 'r')
-    text_list = text.readlines()
-    for index in range(len(text_list)):
-        temp = text_list[index].replace('\n', '').strip('"')
-        text_list[index] = temp
-    return text_list
